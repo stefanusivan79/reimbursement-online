903fea3469c85e4ca0fdd188785707da187b46e1
Stefanus Ivan <stefanusivan79@gmail.com>
2020-12-06 21:09:26 +0700

    add secret vault for slack config

--- reimbursement-online-backend/src/main/java/com/example/reimbursementonlinebackend/util/SlackUtil.java
+++ reimbursement-online-backend/src/main/java/com/example/reimbursementonlinebackend/util/SlackUtil.java
@@ -2,28 +2,41 @@ package com.example.reimbursementonlinebackend.util;
 
 import com.example.reimbursementonlinebackend.service.dto.SlackDTO;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.vault.core.VaultKeyValueOperationsSupport;
+import org.springframework.vault.core.VaultTemplate;
+import org.springframework.vault.support.VaultResponse;
 import org.springframework.web.client.RestTemplate;
 
 @Component
 @Slf4j
 public class SlackUtil {
 
-    @Value("${slack.webhook.url}")
-    private String slackWebhookUrl;
+    private VaultTemplate vaultTemplate;
+
+    public SlackUtil(VaultTemplate vaultTemplate) {
+        this.vaultTemplate = vaultTemplate;
+    }
 
     public void sendMessage(String txt) {
         try {
-            log.info("START SEND MESSAGE");
-            RestTemplate restTemplate = new RestTemplate();
+            VaultResponse vault = vaultTemplate.opsForKeyValue("secret", VaultKeyValueOperationsSupport.KeyValueBackend.KV_2)
+                    .get("eReimbursement");
+            if (null != vault && null != vault.getData() && null != vault.getData().get("slack.webhook.url")) {
+                String slackWebhookUrl = String.valueOf(vault.getData().get("slack.webhook.url"));
+                log.info("START SEND MESSAGE");
+                RestTemplate restTemplate = new RestTemplate();
 
-            HttpEntity<SlackDTO> request = new HttpEntity<>(new SlackDTO(txt));
-            ResponseEntity<String> response = restTemplate.exchange(slackWebhookUrl, HttpMethod.POST, request, String.class);
-            log.info("DONE SEND MESSAGE");
+                HttpEntity<SlackDTO> request = new HttpEntity<>(new SlackDTO(txt));
+                ResponseEntity<String> response = restTemplate.exchange(slackWebhookUrl, HttpMethod.POST, request, String.class);
+                log.info("DONE SEND MESSAGE");
+            }
+            else {
+                log.error("URL NOT FOUND");
+            }
         } catch (Exception e) {
             log.error(e.getLocalizedMessage());
         }
